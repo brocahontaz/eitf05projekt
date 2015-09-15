3a85c6e40065517cc329004a6dff0f6e35eaf763
Johan Andersson <900304@gmail.com>
2015-09-15 14:42:09 +0200

    Some more stuff.

--- index.php
+++ index.php
@@ -9,6 +9,37 @@
 		<link rel="icon" type="image/png" href="../images/favicon.png" />
 	</head>
 	<body>
-		
+		<!-- Wrapper for topbar -->
+		<div id="topbar_wrapper">
+			<!-- Topbar content -->
+			<div id="topbar_content">
+				<!-- Logo box -->
+				<div id="logo_box">
+					Mickey J's Fruit Basket
+				</div>
+				<!-- Login/signin box -->
+				<div id="logsignin_box">
+				Sign in / Register account
+				</div>
+				<!-- Menu container -->
+				<div id="menu_container">
+					<a href="index.php" class="menu_object">
+						HOME
+					</a>
+					<a href="categories.php" class="menu_object">
+						CATEGORIES
+					</a>
+					<a href="products.php" class="menu_object">
+						PRODUCTS
+					</a>
+					<a href="about" class="menu_object">
+						ABOUT / FAQ
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Frontpage color box -->
+		<div id="frontpage_color_box">
+		</div>
 	</body>
 </html>
