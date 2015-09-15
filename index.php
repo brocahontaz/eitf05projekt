76eb74c8e3d54b332cad2728120761259ebfd777
Johan Andersson <900304@gmail.com>
2015-09-15 13:49:14 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			Webshop EITF05
+		</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" />
+		<link rel="icon" type="image/png" href="../images/favicon.png" />
+	</head>
+	<body>
+		
+	</body>
+</html>
