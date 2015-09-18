0d3ac9ade5f2f4565ca1508d91a83b48b8376e55
Johan Andersson <900304@gmail.com>
2015-09-18 07:21:37 +0200

    Started to implement design.

--- index.php
+++ index.php
@@ -2,44 +2,63 @@
 <html>
 	<head>
 		<title>
-			Webshop EITF05
+			thefruitbasket
 		</title>
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 		<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" />
-		<link rel="icon" type="image/png" href="../images/favicon.png" />
+		<link rel="icon" type="image/png" href="./images/favicon.png" />
 	</head>
 	<body>
-		<!-- Wrapper for topbar -->
-		<div id="topbar_wrapper">
-			<!-- Topbar content -->
-			<div id="topbar_content">
-				<!-- Logo box -->
-				<div id="logo_box">
-					Mickey J's Fruit Basket
-				</div>
-				<!-- Login/signin box -->
-				<div id="logsignin_box">
-				Sign in / Register account
-				</div>
-				<!-- Menu container -->
-				<div id="menu_container">
-					<a href="index.php" class="menu_object">
-						HOME
-					</a>
-					<a href="categories.php" class="menu_object">
-						CATEGORIES
-					</a>
-					<a href="products.php" class="menu_object">
-						PRODUCTS
-					</a>
-					<a href="about" class="menu_object">
-						ABOUT / FAQ
-					</a>
+		<div id="main_wrapper">
+			<div id="topbar_wrapper">
+				<div id="topbar_content">
+					<div id="logo_box">
+						<img src="./images/thefruitbasketlogo.png" />
+					</div>
 				</div>
 			</div>
-		</div>
-		<!-- Frontpage color box -->
-		<div id="frontpage_color_box">
+			<div id="menu_wrapper">
+				<div id="menu_container">
+					<!--<div id="menu_object_container">-->
+						<a href="index.php" class="menu_object_active">
+							HOME
+						</a>
+						<a href="index.php" class="menu_object">
+							STORE
+						</a>
+						<a href="index.php" class="menu_object">
+							ABOUT
+						</a>
+						<a href="index.php" class="menu_object">
+							HELP
+						</a>
+					<!--</div>-->
+				</div>
+			</div>
+			<div id="frontpage_big_pic">
+				<div id="frontpage_big_pic_container">
+					<div id="frontpage_big_pic_content">
+						<div id="frontpage_header_box">
+							<span class="header1">
+								FRESH FRUIT
+							</span>
+							<span class="header2">
+								DIRECTLY TO
+								<br />
+								YOUR DOOR
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="color_box_wrapper">
+				<div id="color_box_left">
+				</div>
+				<div id="color_box_right">
+				</div>
+			</div>
+			<div id="footer_wrapper">
+			</div>
 		</div>
 	</body>
 </html>
