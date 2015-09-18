9227ad83a1cc1e4555976ddbeda0b27163a8f8ac
Johan Andersson <johan@rooter.se>
2015-09-18 08:44:20 +0200

    Added more, footer etc.

--- index.php
+++ index.php
@@ -81,6 +81,63 @@
 			<div id="footer_wrapper">
 				<div id="footer_content">
 					<div class="footer_object">
+						<span class="footer_header1">
+							BRINGING FRESHNESS TO THE WORLD
+						</span>
+						
+						<p class="footer_text1">
+							Ever since 2015, we have delivered top notch products to <br />
+							delighted costumers all around the world (mainly Norway).
+							<br /><br />
+							We specialize in the freshest of fruits - a gourmets dream.
+						</p>
+						<p class="copyright">
+							©2015 the<span class="green">fruit</span>basket. ALL RIGHTS RESERVED.
+						</p>
+						<p class="terms">
+							<a href="" class="white_to_green">Privacy policy</a> | <a href="" class="white_to_green">Terms & Conditions</a>
+						</p>
+					</div>
+					<div class="footer_object">
+						<span class="footer_header1">
+							TEAM
+						</span>
+						<p class="footer_text1">
+							<a href="" class="white_to_green">Amanda</a> <br />
+							<a href="" class="white_to_green">Johan</a> <br />
+							<a href="" class="white_to_green">Michael</a> <br />
+							<a href="" class="white_to_green">Therese</a>
+						</p>
+					</div>
+					<div class="footer_object">
+						<span class="footer_header1">
+							CONTACT
+						</span>
+						<p class="footer_text1">
+							E-huset <br />
+							LTH, Lund University <br />
+							Ole Römers Väg 3G <br />
+							223 63 Lund
+						</p>
+					</div>
+					<div class="footer_object">
+						<span class="footer_header1">
+							GET STARTED
+						</span>
+						<p class="footer_text1">
+							Create your thefruitbasket account today,<br /> 
+							and browse our vast line of products.
+						</p>
+						<div id="get_started">
+							<div id="learn_more">
+								LEARN MORE
+							</div>
+							<a href="register.php">
+							<div id="get_started_button">
+								GET STARTED
+							</div>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
